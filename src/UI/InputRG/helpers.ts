7a2f4a6f027b6a2e6d19687b32102622dd186500
Ruslan-Gubin <109853388+Ruslan-Gubin@users.noreply.github.com>
2023-05-03 15:54:11 +0300

    second commit

--- src/UI/InputRG/helpers.ts
+++ src/UI/InputRG/helpers.ts
@@ -7,16 +7,16 @@ const topIconContainer = (
   switch (sizeInput) {
     case "md":
       if (!label) {
-        top = 10;
+        top = 5;
       } else {
-        top = 35;
+        top = 30;
       }
       break;
     case "lg":
       if (!label) {
-        top = 20;
+        top = 15;
       } else {
-        top = 45;
+        top = 40;
       }
       break;
   }
