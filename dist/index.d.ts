6db4b13902bbcfd8615c0334242ad6ecde3820c3
Ruslan-Gubin <109853388+Ruslan-Gubin@users.noreply.github.com>
2023-05-06 10:51:30 +0300

    fix index.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
-export * from './UI';
 import './styles/libs/_index.css';
+import { ButtonRG, InputRG, PaginationRG, CloseIcon, ModalRG } from './UI';
+export { ButtonRG, InputRG, PaginationRG, CloseIcon, ModalRG };
